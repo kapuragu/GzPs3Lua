a0c89ada4882e21ed656bb4e203236c29530da16
kapuragu <38838430+kapuragu@users.noreply.github.com>
2022-01-25 18:06:04 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# GzPs3Lua
+ A dump of Lua scripts from the PS3 version of MGSVGZ, containing original developer comments.
---
+++ as/tpp/ui/Subtitles/script/chapterNameTable_noted.lua
@@ -0,0 +1,78 @@
+if SubtitlesCommand then
+	-- チャプター名設定
+	SubtitlesCommand:SetChapterNameWithID( 1, "COMN" ) --Common
+	SubtitlesCommand:SetChapterNameWithID( 2, "ENMY" ) --Enemy
+	SubtitlesCommand:SetChapterNameWithID( 3, "PWVO" )
+	SubtitlesCommand:SetChapterNameWithID( 4, "HRTE" )
+	SubtitlesCommand:SetChapterNameWithID( 5, "AFGN" ) --Afghanistan
+	SubtitlesCommand:SetChapterNameWithID( 6, "HSPT" ) --Hosptital
+	SubtitlesCommand:SetChapterNameWithID( 7, "EN12" )
+	SubtitlesCommand:SetChapterNameWithID( 8, "OPNG" ) --Opening
+	SubtitlesCommand:SetChapterNameWithID( 9, "PPRG" ) --Paz Prologue (Ground Zeroes)
+	SubtitlesCommand:SetChapterNameWithID( 10, "PRG1" ) --Prologue 1 (Prologue: Awakening)
+	SubtitlesCommand:SetChapterNameWithID( 11, "TRUT" ) --Truth (Truth: The Man Who Sold The World, pre-ambulance crash)
+	SubtitlesCommand:SetChapterNameWithID( 12, "TRU2" ) --Truth 2 (Truth: The Man Who Sold The World, post-ambulance crash)
+	SubtitlesCommand:SetChapterNameWithID( 13, "PGZ1" )
+	SubtitlesCommand:SetChapterNameWithID( 14, "PRSM" )
+	SubtitlesCommand:SetChapterNameWithID( 15, "PRSB" )
+	SubtitlesCommand:SetChapterNameWithID( 16, "PGZ2" )
+	SubtitlesCommand:SetChapterNameWithID( 17, "PRGZ" )
+	SubtitlesCommand:SetChapterNameWithID( 18, "MTBS" ) --Mother Base
+	SubtitlesCommand:SetChapterNameWithID( 19, "FCTE" )
+	SubtitlesCommand:SetChapterNameWithID( 20, "EN01" ) --Enemy
+	SubtitlesCommand:SetChapterNameWithID( 21, "CP01" ) --CP
+	SubtitlesCommand:SetChapterNameWithID( 22, "GNTN" ) --Guantanamo
+	SubtitlesCommand:SetChapterNameWithID( 23, "KATN" )
+	SubtitlesCommand:SetChapterNameWithID( 24, "FIGS" )
+	SubtitlesCommand:SetChapterNameWithID( 25, "RSRB" ) --Traitors' Caravan
+	SubtitlesCommand:SetChapterNameWithID( 26, "KZRQ" ) --Kaz Rescue
+	SubtitlesCommand:SetChapterNameWithID( 27, "KLPW" ) --Kill the POWs (Blood Runs Deep)
+	SubtitlesCommand:SetChapterNameWithID( 28, "HMFC" ) --Human Factory (Voices)
+	SubtitlesCommand:SetChapterNameWithID( 29, "QTRC" ) --Quiet Recover (Cloaked in Silence)
+	SubtitlesCommand:SetChapterNameWithID( 30, "FSAM" ) --Find the SAM (Where Do The Bees Sleep?)
+	SubtitlesCommand:SetChapterNameWithID( 31, "LQRQ" ) --Liquid Rescue (The White Mamba)
+	SubtitlesCommand:SetChapterNameWithID( 32, "FLST" ) --Flow Station (Pitch Dark)
+	SubtitlesCommand:SetChapterNameWithID( 33, "CDTK" ) --Code Talker (Code Talker)
+	SubtitlesCommand:SetChapterNameWithID( 34, "LKLQ" ) --Look Liquid?
+	SubtitlesCommand:SetChapterNameWithID( 35, "BTGR" ) --Battle Gear
+	SubtitlesCommand:SetChapterNameWithID( 36, "NTAT" )
+	SubtitlesCommand:SetChapterNameWithID( 37, "MRDN" )
+	SubtitlesCommand:SetChapterNameWithID( 38, "BKSL" )
+	SubtitlesCommand:SetChapterNameWithID( 39, "GRNM" )
+	SubtitlesCommand:SetChapterNameWithID( 40, "PSDD" ) --Paz's disassociative disorder? (No PSPP/Paz's Phantom Pain)
+	SubtitlesCommand:SetChapterNameWithID( 41, "DDWC" ) --DD Welcome?
+	SubtitlesCommand:SetChapterNameWithID( 42, "DDDP" ) --DD Deploy?
+	SubtitlesCommand:SetChapterNameWithID( 43, "BKDD" ) --Back DD?
+	SubtitlesCommand:SetChapterNameWithID( 44, "HSST" )
+	SubtitlesCommand:SetChapterNameWithID( 45, "TOMB" ) --To Mother Base (Diamond Dogs)
+	SubtitlesCommand:SetChapterNameWithID( 46, "HLPP" ) --Help Prisoner (Over the Fence)
+	SubtitlesCommand:SetChapterNameWithID( 47, "KLPR" ) --Kill PR (A Hero's Way)
+	SubtitlesCommand:SetChapterNameWithID( 48, "DSNE" ) --Destroy Network (C2W)
+	SubtitlesCommand:SetChapterNameWithID( 49, "SVHY" ) --Save Huey (Hellbound)
+	SubtitlesCommand:SetChapterNameWithID( 50, "GRBB" ) --Metallic Archaea (Metallic Archaea)
+	SubtitlesCommand:SetChapterNameWithID( 51, "SHRN" ) --Saheran (Skull Face/Sahelanthropus)
+	SubtitlesCommand:SetChapterNameWithID( 52, "BSBP" ) --Children Escape
+	SubtitlesCommand:SetChapterNameWithID( 53, "CBRP" )
+	SubtitlesCommand:SetChapterNameWithID( 54, "HELI" ) --Helicopter pilot
+	SubtitlesCommand:SetChapterNameWithID( 55, "MGS1" ) --Deja Vu
+	SubtitlesCommand:SetChapterNameWithID( 56, "QTRN" ) --Quiet in the Rain
+	SubtitlesCommand:SetChapterNameWithID( 57, "INFC" ) --Infection (Pandemic event)
+	SubtitlesCommand:SetChapterNameWithID( 58, "FRQK" )
+	SubtitlesCommand:SetChapterNameWithID( 59, "BTDY" ) --Birthday?
+	SubtitlesCommand:SetChapterNameWithID( 60, "QTHL" ) --Quiet helicopter?
+	SubtitlesCommand:SetChapterNameWithID( 61, "INTQ" ) --Interrogate Quiet
+	SubtitlesCommand:SetChapterNameWithID( 62, "LQCS" ) --Liquid and Child Soldier?
+	SubtitlesCommand:SetChapterNameWithID( 63, "DPOC" ) --Pupil Ocelot?
+	SubtitlesCommand:SetChapterNameWithID( 64, "QTCD" ) --Quiet Can Deploy?
+	SubtitlesCommand:SetChapterNameWithID( 65, "GMOV" ) --Game Over
+	SubtitlesCommand:SetChapterNameWithID( 66, "FLKD" ) --Fly Kingdom (Kingdom of the Flies)
+	SubtitlesCommand:SetChapterNameWithID( 67, "KOHY" ) --Kick Out Huey (Recover the AI Pod/Huey's Exile)
+	SubtitlesCommand:SetChapterNameWithID( 68, "SLTR" ) --Slaughter the Infected (Shining Lights)
+	SubtitlesCommand:SetChapterNameWithID( 69, "QGON" ) --Quiet Gone (A Quiet Exit)
+	SubtitlesCommand:SetChapterNameWithID( 70, "ZRNC" ) --Zero Nuclear (Nuclear Disarmament)
+	SubtitlesCommand:SetChapterNameWithID( 71, "ZZZZ" )
+	SubtitlesCommand:SetChapterNameWithID( 72, "ZZZA" )
+	SubtitlesCommand:SetChapterNameWithID( 73, "QPND" ) --Quiet halts a Pandemic?
+	SubtitlesCommand:SetChapterNameWithID( 74, "COMR" ) --Common radio
+	SubtitlesCommand:SetChapterNameWithID( 75, "COMN" ) --Common
+end
